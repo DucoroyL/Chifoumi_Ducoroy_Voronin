a2216076e4f771bea5213b1fb6ac946a4dd8c165
DucoroyL <l.ducoroy@gmail.com>
2017-05-15 11:20:58 +0200

    Logs close #2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+ #include <glog/logging.h>
+ 
 using namespace std;
-int main(){
+int main(int argc, char ** argv){
+	google::SetLogDestination(google::GLOG_INFO, "log_chifoumi");
+	google::InitGoogleLogging(argv[0]); 
 	cout <<"Hello World"<<endl;
+	LOG(INFO) << "Test log Hello World"; 
 	return 0;
 }
